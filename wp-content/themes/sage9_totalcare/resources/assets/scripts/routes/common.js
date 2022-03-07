984b8ef166875872aeb5d411a5ad189afa1ce598
lauraspark <tech@sparkgreen.com.au>
2022-03-07 17:41:44 +1100

    [UPDATE] hamburger menu

--- wp-content/themes/sage9_totalcare/resources/assets/scripts/routes/common.js
+++ wp-content/themes/sage9_totalcare/resources/assets/scripts/routes/common.js
@@ -1,15 +1,21 @@
 export default {
   init() {
     // JavaScript to be fired on all pages
-    window.onscroll = function() {scrollFunction()};
+  window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById('header-logo').style.width = '300px';
-  } else {
-    document.getElementById('header-logo').style.width = '450px';
+  function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      document.getElementById('header-logo').style.width = '300px';
+    } else {
+      document.getElementById('header-logo').style.width = '450px';
+    }
   }
-}
+
+  $(document).ready(function(){
+    $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+      $(this).toggleClass('open');
+    });
+  });
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
--- wp-content/themes/sage9_totalcare/resources/views/partials/header.blade.php
+++ wp-content/themes/sage9_totalcare/resources/views/partials/header.blade.php
@@ -1,6 +1,13 @@
 <header id="header">
   <nav class="navbar justify-content-between px-6">
-    <a class="navbar-brand">Navbar</a>
+    <div class="hamburger-menu">
+      <div id="nav-icon3">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>  
     <img id="header-logo" class="header-logo" src="@asset('images/total-care-logo.png')" alt="Total Care Logo">
   </nav>
 </header>

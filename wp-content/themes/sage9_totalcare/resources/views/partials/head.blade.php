d5569173f05125597ce766fb3e272c8362d95405
lauraspark <tech@sparkgreen.com.au>
2022-03-08 17:07:52 +1100

    [UPDATE] banner buttons

--- wp-content/themes/sage9_totalcare/resources/views/partials/head.blade.php
+++ wp-content/themes/sage9_totalcare/resources/views/partials/head.blade.php
@@ -2,6 +2,6 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&family=Poppins:wght@300;400&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&family=Poppins:wght@300;400;700&display=swap" rel="stylesheet">
   @php wp_head() @endphp
 </head>

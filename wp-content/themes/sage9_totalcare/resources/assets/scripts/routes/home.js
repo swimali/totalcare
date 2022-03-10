e26cec05c9f3328a4674a044c0d252ac56664a65
lauraspark <tech@sparkgreen.com.au>
2022-03-10 12:21:25 +1100

    [UPDATE] Logo section

--- wp-content/themes/sage9_totalcare/resources/assets/scripts/routes/home.js
+++ wp-content/themes/sage9_totalcare/resources/assets/scripts/routes/home.js
@@ -6,6 +6,47 @@ export default {
       slidesToScroll: 1,
       autoplay: true,
       autoplaySpeed: 2000,
+
+      prevArrow:'<img class="a-left control-c prev slick-prev" src="../../wp-content/themes/sage9_totalcare/resources/assets/images/carousel-prev-button.svg">',
+      nextArrow:'<img class="a-right control-c next slick-next" src="../../wp-content/themes/sage9_totalcare/resources/assets/images/carousel-next-button.svg">',
+
+      responsive: [
+        {
+          breakpoint: 2000,
+          settings:{
+            slidesToScroll: 1,
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 1335,
+          settings:{
+            slidesToScroll: 1,
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 1000,
+          settings:{
+            slidesToScroll: 1,
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings:{
+            slidesToScroll: 1,
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 300,
+          settings:{
+            slidesToScroll: 1,
+            slidesToShow: 1,
+          },
+        },
+      ],
     });
   },
   finalize() {
--- wp-content/themes/sage9_totalcare/resources/views/front-page.blade.php
+++ wp-content/themes/sage9_totalcare/resources/views/front-page.blade.php
@@ -10,21 +10,92 @@
       <?php the_field('description'); ?>
     </div>
   </div>
-
-  <div class="carousel-section spacer">
-    <div class="autoplay">
-        <div class="item">
-          1
-        </div>  
-        <div class="item">
-          2
-        </div>  
-        <div class="item">
-          3
-        </div>
-    </div> 
-  </div>
-
 </div>
+
+<div class="carousel-section spacer">
+  <div class="dark-blue-background">
+    <div class="container slick-section">
+      <div class="autoplay">
+        <?php if( have_rows('home_carousel') ):
+          $i = 0;
+          while( have_rows('home_carousel') ) : the_row();
+          $home_image = get_sub_field('image');
+        ?>
+
+        <div class="item">
+          <a href="<?php the_sub_field('link'); ?>">
+              <img class="carousel-image" src="<?php echo esc_url($home_image['url']); ?>"
+                alt="<?php echo esc_url($home_image['alt']); ?>">
+          </a>
+        </div>
+
+        <?php $i++;
+        endwhile; ?>
+        <?php endif; ?>
+      </div> 
+    </div>
+  </div>
+</div>
+
+<div class="left-image-section">
+  <div class="row no-gutters">
+  <?php 
+    if( have_rows('left_image_section') ):
+    while( have_rows('left_image_section') ) : the_row(); ?>
+    <div class="col-md-3">
+      <?php $image = get_sub_field('left_image'); ?>
+      <img class="image w-100" src="<?php echo esc_url( $image['url'] ); ?>" alt="<?php echo esc_url( $image['alt'] ); ?>" />
+    </div>
+    <div class="col-md-9 spacer">
+      <?php the_sub_field('description'); ?>
+    </div>
+  <?php endwhile;
+  endif; ?>  
+  </div>
+</div>
+
+<div class="right-image-section">
+  <div class="row no-gutters">
+  <?php 
+    if( have_rows('right_image_section') ):
+    while( have_rows('right_image_section') ) : the_row(); ?>
+    <div class="col-md-9 spacer">
+      <?php the_sub_field('description'); ?>
+    </div>
+    <div class="col-md-3">
+      <?php $image = get_sub_field('image'); ?>
+      <img class="image w-100" src="<?php echo esc_url( $image['url'] ); ?>" alt="<?php echo esc_url( $image['alt'] ); ?>" />
+    </div>
+  <?php endwhile;
+  endif; ?>  
+  </div>
+</div>
+
+<div class="container white-section spacer">
+<?php 
+    if( have_rows('white_section') ):
+    while( have_rows('white_section') ) : the_row(); ?>
+
+    <?php the_sub_field('description'); ?>
+
+    <div class="row no-gutters justify-content-md-center">
+    <?php 
+    if( have_rows('logos') ):
+    while( have_rows('logos') ) : the_row(); 
+    $image = get_sub_field('image');
+    ?>
+    
+      <div class="col-md-3">
+        <img class="image w-100" src="<?php echo esc_url( $image['url'] ); ?>" alt="<?php echo esc_url( $image['alt'] ); ?>" />
+      </div>
+
+    <?php endwhile;
+    endif; ?>  
+    </div>
+
+    <?php endwhile;
+  endif; ?>  
+</div> 
+
 
 @endsection
